c6faa406aba8da9390e1ed3d2ba7be9f0988d54f
bussele <101179918+bussele@users.noreply.github.com>
2022-04-08 11:00:44 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,50 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port = 3000;
+
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log("server running");
+    console.log(`running on localhost: ${port}`);
+};
+
+
+// GET
+app.get('/all', function (req, res) {
+    res.send(projectData);
+});
+
+// POST
+app.post('/addWeatherData', (req, res)=> {
+    projectData['date'] = req.body.date;
+    projectData['temperature'] = req.body.temperature;
+    projectData['userResponse'] = req.body.userResponse;
+    console.log(req.body);
+    console.log(projectData);
+    res.send(projectData);
+}); 
+
